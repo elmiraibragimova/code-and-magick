7c4432810b5cbf146bc7604267a4534825dff057
Elmira Ibragimova <elmira.ibragimova28@gmail.com>
2016-03-26 22:59:58 +0300

    correct code style

--- build/js/check.js
+++ build/js/check.js
@@ -1,37 +1,36 @@
 var getMessage = function(a, b) {
+  var i;
 
-	var i;
+  if (typeof a === 'boolean') {
+    if (a) {
+      return 'Я попал в ' + b;
+    }
 
-	if ( typeof a === 'boolean' ) {
-		if (a) return 'Я попал в ' + b;
+    return 'Я никуда не попал';
+  }
 
-		return 'Я никуда не попал';
-	}
+  if (typeof a === 'number') {
+    return 'Я прыгнул на ' + a * 100 + ' сантиметров';
+  }
 
-	if( typeof a === 'number' ) {
-		return 'Я прыгнул на ' + a * 100 + ' сантиметров';
-	}
+  if(a) {
+    if(b) {
+      var minLength = Math.min(a.length, b.length);
+      var length = 0;
 
-	if(a) {
-		if(b) {
+      for ( i = 0; i < minLength; i++ ) {
+        length += a[i] * b[i];
+      }
 
-			var minLength = Math.min(a.length, b.length);
-			var length = 0;
+      return 'Я прошёл ' + length + ' метров';
+    } else {
+      var sum = 0;
 
-			for ( i = 0; i < minLength; i++ ) {
-				length += a[i] * b[i];
-			}
+      for (   i = 0; i < a.length; i++ ) {
+        sum += a[i];
+      }
 
-			return 'Я прошёл ' + length + ' метров';
-
-		} else {
-				var sum = 0;
-
-				for (   i = 0; i < a.length; i++ ) {
-					sum += a[i];
-				}
-
-				return 'Я прошёл ' + sum + ' шагов';
-		}
-	}
+      return 'Я прошёл ' + sum + ' шагов';
+    }
+  }
 }
